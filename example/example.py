526e14a8576665180975fc7ccdcf273d83626711
Muhammad Nawaz <m.nawaz2003@gmail.com>
2023-12-09 12:50:44 +0500

    Updated path

--- example/example.py
+++ example/example.py
@@ -1,5 +1,8 @@
 import oasvalidator
+import os
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+json_path = os.path.join(dir_path, "../thirdparty/cpp-oasvalidator/data/openAPI_example.json")
 validators = oasvalidator.init("thirdparty/cpp-oasvalidator/data/openAPI_example.json")
 error, error_msg = validators.validate_route("GET", "/v1/pets")
 
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,8 +1,15 @@
+import os
 import oasvalidator
 
 
 def test_validate():
-    validators = oasvalidator.init("thirdparty/cpp-oasvalidator/data/openAPI_example.json")
+    # Get the directory of the current script
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    # Construct the path to the JSON file
+    json_path = os.path.join(dir_path, "../thirdparty/cpp-oasvalidator/data/openAPI_example.json")
+
+    # Use the constructed path in your test
+    validators = oasvalidator.init(json_path)
     error, error_msg = validators.validate_route("GET", "/v1/pets")
     assert error == oasvalidator.ValidationError.INVALID_ROUTE
-
